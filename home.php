f31d8dc49d3cfec76c365be1d9c8e636a519673e
Kevoh <40100329+kev254@users.noreply.github.com>
2021-04-06 23:15:25 +0300

    Committed by kevin

---
+++ home.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Beta Restuarant</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+	<style type="text/css">
+		h2, caption{
+			color: red;
+		}
+		img{
+			border-radius: 60px;
+			
+	</style>
+		}
+</head>
+<body>
+	<center>
+	<h2> Beta Restaurant</h2>
+	<br><br>
+	<div class="row">
+			<div class="col-4">
+					<img src="https://cdn.pixabay.com/photo/2020/05/22/12/16/chicken-drumsticks-5205207__340.jpg" height="350px" width="350px">
+			</div>
+
+		<div class="col">
+			<h5>  Our Food Menu</h5>
+	
+   <table class="table table-borderless">
+   	<caption>End of Menu</caption>
+  <thead>
+    <tr>
+      <th scope="col">Food Image</th>
+      <th scope="col">Name</th>
+      <th scope="col">Descrption</th>
+      <th scope="col">Cost (Kshs.)</th>
+      <th scope="col">Action</th>
+
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row"><div class="imageform"><img src="https://cdn.pixabay.com/photo/2016/03/05/19/02/abstract-1238247__340.jpg" height="80px" width="80px"></div></th>
+      <td>American Barbaque</td>
+      <td>Yummy breakfast barbabcue</td>
+      <td>Ksh. 250</td>
+      <td><button type="button" class="btn btn-primary">Order</button></td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="https://cdn.pixabay.com/photo/2016/07/31/18/00/chicken-1559579__340.jpg" height="80px" width="80px"></th>
+      <td>Roated chicken</td>
+      <td>Roasted loacl chiken</td>
+      <td>Kshs. 550</td>
+      <td><button type="button" class="btn btn-primary">Order</button></td>
+    </tr>
+    <tr>
+      <th scope="row"><img src="https://cdn.pixabay.com/photo/2017/01/22/19/12/pizza-2000600__340.jpg" height="80px" width="80px"></th>
+      <td>Pizza</td>
+      <td>Tasty Holland Pizza</td>
+      <td>Ksh. 1500</td>
+      <td><button type="button" class="btn btn-primary">Order</button></td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</center>
+	
+
+</body>
+</html>
